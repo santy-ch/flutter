19f3b828f37fdad3f0023557c9790deadba32199
santiago chiluisa <santoch93@hotmail.com>
2021-03-22 23:12:32 -0400

    segundo avance

--- lib/ejemplos.dart
+++ lib/ejemplos.dart
@@ -15,6 +15,20 @@ class Ejemplos extends StatelessWidget {
           children: [
             Container(
               child: Gradiente(),
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+            child: ListView(
+            children:<Widget>[
+              Image.network("https://image.slidesharecdn.com/exposicion10acostaanapaulaherrerabethsabe-160526180615/95/ingenieria-social-18-638.jpg?cb=1464286309"),
+              SizedBox(height: 20.0,),
+              Image.network("https://slideplayer.es/slide/3341836/11/images/6/Ejemplos+de+ingenier%C3%ADa+social.jpg"),
+              SizedBox(height: 20.0,),
+              Image.network("https://deepwebiupsm.files.wordpress.com/2016/06/deepweb11.jpg?w=736"),
+              SizedBox(height: 20.0,),
+              Image.network("https://image.slidesharecdn.com/pampasegsocialengtntspa-101205161302-phpapp02/95/ingenieria-social-una-tecnica-no-tan-tecnica-pampaseg-2010-la-pampa-argentina-17-728.jpg?cb=1291566258")
+          ],
+        ),
             )
         ],
         ),
